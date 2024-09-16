81ca5cd5dbb94155e87a3a0b328f4dbecee79397
emrz16 <e.rodriguezz@uniandes.edu.co>
2024-09-16 17:34:24 -0600

    values

--- control/monitor.py
+++ control/monitor.py
@@ -20,7 +20,7 @@ def analyze_data():
 
     data = Data.objects.filter(
         base_time__gte=datetime.now() - timedelta(hours=1))
-    aggregation = data.annotate(check_value=Avg('avg_value'), variance_value=Variance('avg_value')) \
+    aggregation = data.annotate(check_value=Avg('avg_value'), variance_value=Variance('values')) \
         .select_related('station', 'measurement') \
         .select_related('station__user', 'station__location') \
         .select_related('station__location__city', 'station__location__state',
